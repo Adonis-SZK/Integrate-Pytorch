abb64667edfc827a848783c87d899824e6770b2f
TWK <109967047+TWK2022@users.noreply.github.com>
2022-08-02 10:58:57 +0800

    Update run.py

--- train/run.py
+++ train/run.py
@@ -47,7 +47,8 @@ parser.add_argument('--OD_smooth', default=[0.05,0.95], type=list, help='|OD标�
 parser.add_argument('--OD_output', default=[[80,40,20],[3]], type=list, help='|OD输出形状|')
 parser.add_argument('--OD_anchor', default=[[[10,13],[16,30],[33,23]],[[30,61],[62,45],[59,119]],[[116,90],[156,198],[373,326]]],type=list, help='|OD先验框|')
 parser.add_argument('--OD_confidence_threshold', default=0.8, type=float, help='|OD准确率计算的置信度阈值，基准为0.5|')
-parser.add_argument('--OD_plot', default=[True,3], type=list,help='|OD是否画出检测的图片及画出的图片数量|')
+parser.add_argument('--OD_plot', default=[True,3], type=list, help='|OD是否画出并保存测试的图片和画出的图片数量|')
+parser.add_argument('--OD_plot_show', default=False, type=list, choices=[True,False], help='|OD是否在页面显示非极大值抑制前和后的图片|')
 parser.add_argument('--OD_plot_screen', default=300, type=int, help='|OD非极大值抑制前根据置信度排名筛选出的框数|')
 parser.add_argument('--OD_plot_threshold', default=0.2, type=float, help='|OD画图时消除同类别重叠框的iou阈值，越低筛选出的框越少|')
 
