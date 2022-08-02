09adfe39251c8192887006408440a74a5e77cac1
TWK <109967047+TWK2022@users.noreply.github.com>
2022-08-02 16:01:15 +0800

    Update save_get.py

--- train/block/save_get.py
+++ train/block/save_get.py
@@ -1,8 +1,9 @@
 import torch
 def save_get(args,model):
-    dict_choice={'pkl':'save(args)._pkl(model)'
-                }
-    return eval(dict_choice[args.model_save])
+    if args.model_save[0]:
+        dict_choice={'pkl':'save(args)._pkl(model)'
+                    }
+        return eval(dict_choice[args.model_save[1]])
 
 class save(object):
     def __init__(self,args):
