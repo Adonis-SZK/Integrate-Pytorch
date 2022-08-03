1372c49cefac8b6cc974fa69d901b98b526d5c07
TWK <109967047+TWK2022@users.noreply.github.com>
2022-08-03 15:05:07 +0800

    Update test_get.py

--- train/block/test_get.py
+++ train/block/test_get.py
@@ -150,7 +150,7 @@ class OD_dataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         args=self.args
         dict_dataset=self.dict_dataset
-        test=torch.tensor(dict_dataset['img_test'][index]).to(args.device)
+        test=torch.tensor(dict_dataset['img_test'][index]/255).type(torch.float32).to(args.device)
         list_mask=[0 for i in range(len(args.OD_output[0]))]
         list_label=[0 for i in range(len(args.OD_output[0]))]
         for i,stride in enumerate(dict_dataset['stride']):
