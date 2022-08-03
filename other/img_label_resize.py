62da060eb9d2c3f9a42a8ebfc5e87ed98b823b7c
TWK <109967047+TWK2022@users.noreply.github.com>
2022-08-03 10:39:41 +0800

    Update img_label_resize.py

--- other/img_label_resize.py
+++ other/img_label_resize.py
@@ -16,8 +16,8 @@ if not os.path.exists(save_label):
     os.makedirs(save_label)
 # -------------------------------------------------------------------------------------------------------------------- #
 #程序
-dir_img = os.listdir(path_img)
-dir_label = os.listdir(path_label)
+dir_img = sorted(os.listdir(path_img))
+dir_label = sorted(os.listdir(path_label))
 for i in range(len(dir_img)):
     img=cv2.imread(path_img+'/'+dir_img[i])
     label=pd.read_csv(path_label+'/'+dir_label[i])
